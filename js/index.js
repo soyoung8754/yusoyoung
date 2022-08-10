e37c37d41328ad3f1bb2d8a725de7edf98adc910
soyoung8754 <108649827+soyoung8754@users.noreply.github.com>
2022-08-10 17:01:37 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	
 	//상단메뉴 (브라우저 크기 500픽셀 이상에서 볼때):주메뉴에 마우스를 댈때 (웹접근성강화)
-	if($(document).width()>500){
+	if($(window).width()>500){
 		$("header li> a").on("focus mouseenter", function(){
 			//모든 하위메뉴를 안보이게 한 후
 			//$("header li nav").stop().slideUp();
@@ -20,15 +20,16 @@ $(document).ready(function(){
 	
 	//하단 드롭다운메뉴 기능
 	$("#partner").click(function(){
-		$(this).next().slideDown();
+		$(this).next().stop().slideDown();
 	});
 	//하단 드롭다운메뉴영역 벗어나면 올라간다.
 	$("#partner+nav").mouseleave(function(){
 		$(this).slideUp();
 	});
+
+
 	
-	////////////////////////////////
-	
+	///////////////////////////////////////////////////////
 	//모바일에서 햄버거 아이콘 클릭시 메뉴가 왼쪽에서 나타남
 	if( $(window).width() <= 500 ){
 	
@@ -38,26 +39,48 @@ $(document).ready(function(){
 				//햄버거 안보인다.
 				$(this).hide();
 				$("header>nav").stop().animate({left:0 });
+				
+				$("body").css("overflow",'hidden');
 		});
-	
-	 //모바일에서 메뉴 항목을 클릭하면 메뉴가 왼쪽으로 사라진다.
-		$("header>nav nav, header>nav a:not(.arr), .close").on("click mouseleave" , function(){
-				//닫기(X) 안보인다
-				$("header button.close").hide();
-				//햄버거 보인다.
+		
+		$("header button.close").click(function(){
+				//닫기(X) 보인다
 				$("header button.open").show();
-				//메뉴가 왼쪽으로 사라진다
-				$("header>nav").stop().animate({left:"-100vw"});
-				$(this).stop().slideUp();
+				//햄버거 안보인다.
+				$("header button.close").hide();
+				$("header>nav").stop().animate({left: "-100vw" });
+				
+				$('header li>a.arr').next().stop().slideUp();
+				$('header li>a.arr').removeClass('arr2').addClass('arr1');
+				
+				$("body").css("overflow",'auto');
 		});
 	
-		 //모바일에서 메뉴 항목에서 주메뉴 오른쪽 화살표 클릭하면 서브메뉴 닫힌다
-		$("header a.arr").click(function(){
-			$(this).next().slideToggle();
-		});  
+		
+		$('header li>a.arr').click(function(){			
+			if(  $(this).hasClass('arr1') == true ){
+					$('header li>a.arr2').next().stop().slideUp();					
+					$(this).next().stop().slideDown();
+					$('header li>a.arr').removeClass('arr2').addClass('arr1');
+					$(this).removeClass('arr1').addClass('arr2');
+			} else {	
+					$(this).removeClass('arr2').addClass('arr1');
+					$(this).next().stop().slideUp();
+			}					
+		});
+		
+		$("header> nav> div, header  nav>a").click(function(){
+				$("header button.open").show();
+				$("header button.close").hide();
+				$("header>nav").stop().animate({left: "-100vw" });
+				
+				$('header li>a.arr').next().stop().slideUp();
+				$('header li>a.arr').removeClass('arr2').addClass('arr1');	
+				
+				$("body").css("overflow",'auto');
+		});
+		
+		
 	}///////////////모바일: 끝
 	
-	
-	
-	
-});////////////////전체 끝부분
+});//////////////////////////////////////전체 끝부분
